2a1755c6e78e1586c1f20f1a4b77e176da0dcbaf
santiagocarmoa <santiago.carmonab@udea.edu.co>
2020-08-13 09:19:09 -0500

    Se suben los primeros 2 archivos js y HTML del seguimiento al tutorial sobre vuejs

---
+++ 01.js
@@ -0,0 +1,12 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo con Vue',
+        frutas: [
+            {nombre: 'Pera', cantidad: 12},
+            {nombre: 'Manzana', cantidad: 0},
+            {nombre: 'Platano', cantidad: 14}
+        ]
+    } 
+
+})
