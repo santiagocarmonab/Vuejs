e38135265b2bb705e8e70789c9e84aaaa90b3fcd
santiagocarmoa <santiago.carmonab@udea.edu.co>
2020-08-13 14:56:37 -0500

    Nueva actualizacion de los archivos 01, ahora se usan computed: para sumar la cantidad total de frutas ante cualquier cambio que se represente de estos valores, tambien desde pantalla se tiene un boton para sumar frutas una unidad a la vez

--- 01.js
+++ 01.js
@@ -7,14 +7,26 @@ const app = new Vue ({
             {nombre: 'Manzana', cantidad: 0},
             {nombre: 'Platano', cantidad: 14}
         ],
-        nuevaFruta: '' 
+        nuevaFruta: '', 
+        total : 0
     },
     
     methods: {
         agregarFruta (){
             this.frutas.push({
                 nombre : this.nuevaFruta, cantidad:0
-            })
+            });
+            this.nuevaFruta='';
+        }      
+    },
+    computed: {
+        sumarFrutas(){
+            this.total=0;
+            for(fruta of this.frutas){
+                this.total = this.total + fruta.cantidad;
+            }
+
+            return this.total;
         }
     }
 
